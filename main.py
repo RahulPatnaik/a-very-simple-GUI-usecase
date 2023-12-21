2b3cd57facdd0781345455f32d9a2564c74a79c8
RahulPatnaik <142738799+RahulPatnaik@users.noreply.github.com>
2023-12-21 10:27:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ from tkinter import *
 from tkinter import simpledialog, messagebox
 
 app = Tk()
-app.title("A simple GUI app")
+app.title("My GUI App")
 
-# Set a custom color scheme
-app.configure(bg='#E0E0E0')
-label_color = '#333333'
+app.configure(bg='#1E1E1E')
+label_color = '#FFFFFF'
 button_color = '#4CAF50'
-entry_color = '#FFFFFF'
+entry_color = '#333333'
+font_style = ('Arial', 14, 'bold')  # Increased font size for a more professional look
+user_input_color = 'white'
 
-app.geometry("500x400+400+200")
+app.geometry("700x600+300+150")  # Adjusted window size for better layout
 
-# Function to calculate the golden ratio
 def get_info():
     user_input = E1.get()
     try:
@@ -20,36 +20,32 @@ def get_info():
         for i in range(0, int(user_input)):
             fiblist.append(fiblist[i] + fiblist[i + 1])
         g_ratio = [fiblist[i] / float(fiblist[i - 1]) for i in range(2, len(fiblist))]
-        result_label.config(text="Golden ratio is: " + str(g_ratio))
+        result_label.config(text="Golden ratio is: " + str(g_ratio[-1]), fg=user_input_color)
+        fibonacci_label.config(text="Fibonacci series: " + str(fiblist), fg=user_input_color)
     except ValueError:
-        result_label.config(text="Invalid input. Enter a number.")
+        result_label.config(text="Invalid input. Enter a number.", fg='red')
 
-# Function to handle list operations
 def list_entry():
     try:
-        n = int(iterations.get())
+        n = int(E1.get())  
         make_list(n)
     except ValueError:
         messagebox.showerror("Error", "Enter a valid number.")
 
-# Function to create a list
 def make_list(n):
     l = [" "]*n
     update_listbox(l)
 
-# Function to update the listbox
 def update_listbox(u_list):
     listbox.delete(0, END)
     for i in u_list:
         listbox.insert(END, i)
 
-# Function to append a value to the list
 def append_value():
     val = input_entry.get()
     l.append(val)
     update_listbox(l)
 
-# Function to insert a value into the list
 def insert_value():
     global l
     index = listbox.curselection()
@@ -61,7 +57,6 @@ def insert_value():
     else:
         messagebox.showerror("Error", "Please select an item in the listbox.")
 
-# Function to remove a value from the list
 def remove_value():
     global l
     index = listbox.curselection()
@@ -72,45 +67,59 @@ def remove_value():
     else:
         messagebox.showerror("Error", "Please select an item in the listbox.")
 
-# Labels and buttons with the updated style
-L1 = Label(app, text="Numerical Input for Golden Ratio", bg=label_color, fg='white', font=('Arial', 12))
-L1.pack(pady=20)
+instructions = """
+Welcome to My GUI App!
 
-B1 = Button(app, text="Calculate Golden Ratio", command=get_info, bg=button_color, fg='white', font=('Arial', 12))
-B1.pack(pady=20)
+1. Enter a number in the field below and click 'Calculate Golden Ratio' to see the golden ratio sequence.
 
-E1 = Entry(app, bg=entry_color, font=('Arial', 12))
-E1.pack(pady=20)
+2. Use the list operations section:
+   - Enter a value in 'Enter a value' field.
+   - Click 'Append Value' to add it to the list.
+   - Select an item in the listbox and click 'Insert Value' to insert a value.
+   - Select an item in the listbox and click 'Remove Value' to remove it.
 
-result_label = Label(app, text="", bg=label_color, fg='white', font=('Arial', 12))
+Have fun exploring the app!
+"""
+
+instructions_label = Label(app, text=instructions, bg='#1E1E1E', fg=label_color, font=font_style, justify=LEFT)
+instructions_label.pack(pady=15)
+
+L1 = Label(app, text="Numerical Input for Golden Ratio", bg='#1E1E1E', fg=label_color, font=font_style)
+L1.pack(pady=5)
+
+B1 = Button(app, text="Calculate Golden Ratio", command=get_info, bg=button_color, fg='white', font=font_style, relief=FLAT)
+B1.pack(pady=5)
+
+E1 = Entry(app, bg=entry_color, font=font_style, relief=FLAT, bd=2, fg=user_input_color)
+E1.pack(pady=5)
+
+result_label = Label(app, text="", bg='#1E1E1E', fg=label_color, font=font_style)
 result_label.pack()
 
-iter_label = Label(app, text="Number of iterations:", bg=label_color, fg='white', font=('Arial', 12))
-iter_label.pack()
+fibonacci_label = Label(app, text="", bg='#1E1E1E', fg=label_color, font=font_style)
+fibonacci_label.pack()
 
-iterations = Entry(app, bg=entry_color, font=('Arial', 12))
-iterations.pack()
+# Separating list manipulation part with a heading
+list_heading = Label(app, text="List Manipulation", bg='#1E1E1E', fg=label_color, font=font_style)
+list_heading.pack(pady=10)
 
-done = Button(app, text="Done", command=list_entry, bg=button_color, fg='white', font=('Arial', 12))
-done.pack()
+listbox = Listbox(app, bg=entry_color, font=font_style, relief=FLAT, bd=2, fg=user_input_color)
+listbox.pack(pady=5)
 
-listbox = Listbox(app)
-listbox.pack(pady=10)
-
-input_label = Label(app, text="Enter a value:", bg=label_color, fg='white', font=('Arial', 12))
+input_label = Label(app, text="Enter a value:", bg='#1E1E1E', fg=label_color, font=font_style)
 input_label.pack()
 
-input_entry = Entry(app, bg=entry_color, font=('Arial', 12))
-input_entry.pack()
+input_entry = Entry(app, bg=entry_color, font=font_style, relief=FLAT, bd=2, fg=user_input_color)
+input_entry.pack(pady=5)
 
-append = Button(app, text="Append Value", command=append_value, bg=button_color, fg='white', font=('Arial', 12))
-append.pack()
+append = Button(app, text="Append Value", command=append_value, bg=button_color, fg='white', font=font_style, relief=FLAT)
+append.pack(pady=5)
 
-insert = Button(app, text="Insert Value", command=insert_value, bg=button_color, fg='white', font=('Arial', 12))
-insert.pack()
+insert = Button(app, text="Insert Value", command=insert_value, bg=button_color, fg='white', font=font_style, relief=FLAT)
+insert.pack(pady=5)
 
-remove = Button(app, text="Remove Value", command=remove_value, bg=button_color, fg='white', font=('Arial', 12))
-remove.pack()
+remove = Button(app, text="Remove Value", command=remove_value, bg=button_color, fg='white', font=font_style, relief=FLAT)
+remove.pack(pady=5)
 
 l = []
 app.mainloop()
